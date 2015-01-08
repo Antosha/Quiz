d41f34110d68e7f9df7d13aeeba679a2cd70dec8
Anton Bondarev <Bondarev@Antons-MacBook-Air.local>
2015-01-09 00:44:28 +0200

    tiny change at the title

--- js/slickQuiz-config.js
+++ js/slickQuiz-config.js
@@ -4,7 +4,7 @@
 
 var quizJSON = {
     "info": {
-        "name":    "Test Your Knowledge!!",
+        "name":    "Software Testing Skills ELance Test",
         "main":    "<p>Let's check your QA Knowledge</p>",
         "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
         "level1":  "Jeopardy Ready",
@@ -15,12 +15,12 @@ var quizJSON = {
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "What number is the letter A in the English alphabet?",
+            "q": "True or false? Zero and one are typically in the same equivalence class.",
             "a": [
-                {"option": "8",      "correct": false},
-                {"option": "14",     "correct": false},
-                {"option": "1",      "correct": true},
-                {"option": "23",     "correct": false} // no comma here
+                //{"option": "8",      "correct": false},
+                //{"option": "14",     "correct": false},
+                {"option": "False",      "correct": true},
+                {"option": "True",     "correct": false} // no comma here
             ],
             "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
             "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
