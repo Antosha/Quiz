62ac232712a5f5a50b2bd43995d9b6d3168649b5
Anton Bondarev <Bondarev@Antons-MacBook-Air.local>
2015-01-09 00:13:03 +0200

    Changed main parameter at info

--- js/slickQuiz-config.js
+++ js/slickQuiz-config.js
@@ -5,7 +5,7 @@
 var quizJSON = {
     "info": {
         "name":    "Test Your Knowledge!!",
-        "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
+        "main":    "<p>Let's check your QA Knowledge</p>",
         "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
         "level1":  "Jeopardy Ready",
         "level2":  "Jeopardy Contender",
